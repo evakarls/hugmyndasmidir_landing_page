66edec831220bd1a4cb7a8ee45d4bc2a4cc22402
Eva Karlsdottir <evakarlsdottir@gmail.com>
2022-08-28 14:53:05 +0000

    feat: landing page first draft

---
+++ index.js
@@ -0,0 +1,57 @@
+const scrollElements = document.querySelectorAll(".scrollable");
+
+var throttleTimer;
+
+const throttle = (callback, time) => {
+  if (throttleTimer) return;
+
+  throttleTimer = true;
+  setTimeout(() => {
+    callback();
+    throttleTimer = false;
+  }, time);
+};
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el);
+    }
+  });
+};
+var timer = 0;
+var count = 0;
+var scroll = 0;
+
+window.addEventListener("scroll", () => {
+  throttle(() => {
+    handleScrollAnimation();
+  }, 250);
+});
